1f8b90126f9a06d9511fbc5eaf85327db9159ff9
Andreas Hosbach <andreas.hosbach@gmail.com>
2020-02-15 15:48:44 +0100

    Renamed Cucumber test runner so it will be executed with junit

--- src/test/java/CucumberTestRunner.java
+++
@@ -1,8 +0,0 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features", glue= "step_definitions", plugin = { "json:target/cucumber.json"})
-public class CucumberTestRunner {
-}
